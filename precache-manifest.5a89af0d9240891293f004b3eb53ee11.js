5c89aacb67f8721137b13f5ec2e540b5fe0702b0
Eduardo Muñoz <eduardo@MacBook-Pro-de-Eduardo.local>
2019-01-13 10:25:39 +0100

    Updates

---
+++ precache-manifest.5a89af0d9240891293f004b3eb53ee11.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = [
+  {
+    "revision": "5d5d9eefa31e5e13a6610d9fa7a283bb",
+    "url": "/personal/static/media/logo.5d5d9eef.svg"
+  },
+  {
+    "revision": "bd80bb5a4057e7b82a0f",
+    "url": "/personal/static/js/runtime~main.bd80bb5a.js"
+  },
+  {
+    "revision": "85aa5f4f1545de695c4c",
+    "url": "/personal/static/js/main.85aa5f4f.chunk.js"
+  },
+  {
+    "revision": "4a34e45d34cafb7623bd",
+    "url": "/personal/static/js/1.4a34e45d.chunk.js"
+  },
+  {
+    "revision": "85aa5f4f1545de695c4c",
+    "url": "/personal/static/css/main.fbb8e5e7.chunk.css"
+  },
+  {
+    "revision": "ef27fc1c08f6e2f09afacac395af5009",
+    "url": "/personal/index.html"
+  }
+];
